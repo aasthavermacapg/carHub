b77e0a9f090a37b979fe6c985bf486c068e68881
aasthavermacapg <vaastha16@gmail.com>
2023-10-24 18:22:22 +0530

    car filter design(160)

--- force-app/main/default/lwc/carFilter/carFilter.js
+++ force-app/main/default/lwc/carFilter/carFilter.js
@@ -1,3 +1,49 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
+import { getObjectInfo, getPicklistValues } from 'lightning/uiObjectInfoApi';
+import CAR_OBJECT from '@salesforce/schema/Car__c'
 
-export default class CarFilter extends LightningElement {}
+// car schema
+import CATEGORY_FIELD from '@salesforce/schema/Car__c.Category__c';
+import MAKE_FIELD from '@salesforce/schema/Car__c.Make__c';
+
+// constants
+const CATEGORY_ERROR = 'Error loading Categories';
+const MAKE_ERROR = 'Error loading Make type';
+
+export default class CarFilter extends LightningElement {
+    filters = {
+        searchKey : '',
+        maxPrice : 999999
+    }
+    categoryError = CATEGORY_ERROR;
+    makeError = MAKE_ERROR;
+    // fetching category picklist
+    @wire(getObjectInfo, {objectApiName : CAR_OBJECT})
+    carObjectInfo
+
+    @wire(getPicklistValues, {
+        recordTypeId : '$carObjectInfo.data.defaultRecordTypeId',
+        fieldApiName : CATEGORY_FIELD
+    })categories
+
+    // fetching make picklist
+    @wire(getPicklistValues, {
+        recordTypeId : '$carObjectInfo.data.defaultRecordTypeId',
+        fieldApiName : MAKE_FIELD
+    })makeType
+    
+    // search key handler
+    handleSearchKeyChange(event){
+        console.log(event.target.value);
+        this.filters = {...this.filters, "searchKey" : event.target.value};
+    }
+    handleMaxPriceChange(event){
+        console.log(event.target.value);
+        this.filters = {...this.filters, "maxPrice" : event.target.value};
+    }
+    handleCheckbox(event){
+        const {name, value} = event.target.dataset;
+        console.log("name : ", name);
+        console.log("value : ",value);
+    }
+}
